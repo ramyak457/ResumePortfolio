f2079acb637a17bd89ecb3918c1b18d4119e82b5
Ramya Kumar <ramya04.kumar@gmail.com>
2024-06-12 16:18:47 -0700

    Contact page send email

--- ResumePortfolio/Controllers/HomeController.cs
+++ ResumePortfolio/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace ResumePortfolio.Controllers
 {
@@ -24,8 +27,27 @@ namespace ResumePortfolio.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string firstname,string lastname,string email,string body)
+        {
+            try
+            {
+                string useremail = "ramya04.kumar@gmail.com";
+                string subject = "Email From " + firstname + lastname;
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential(useremail, "wpektkdrfvmpfwwq"),
+                    EnableSsl = true
+                };
+                client.Send(email, useremail, subject, body);
+                ViewData["success"] = "Email sent successfully";
+            }
+            catch (Exception ex)
+            {
+                ViewData["error"] = ex.Message;
+            }                       
             return View();
         }
     }
